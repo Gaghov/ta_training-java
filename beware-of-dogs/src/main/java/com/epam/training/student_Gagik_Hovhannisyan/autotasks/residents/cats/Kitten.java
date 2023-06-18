0993936ca02a9a656720f6c68f7be3ed7be342c8
Gaghov <133580849+Gaghov@users.noreply.github.com>
2023-06-19 00:23:16 +0400

    Add files via upload
    Practical Task "Beware of Dogs"

---
+++ beware-of-dogs/src/main/java/com/epam/training/student_Gagik_Hovhannisyan/autotasks/residents/cats/Kitten.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_Gagik_Hovhannisyan.autotasks.residents.cats;
+
+public class Kitten extends Cat {
+
+    public Kitten(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Kitten " + name;
+    }
+}
