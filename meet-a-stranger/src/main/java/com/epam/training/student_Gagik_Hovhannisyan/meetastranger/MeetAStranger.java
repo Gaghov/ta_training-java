cacf4dcd9e2e72cab5006f291df51bed715cb488
Gaghov <133580849+Gaghov@users.noreply.github.com>
2023-07-03 00:17:59 +0400

    Practical Task "Meet a Stranger"

---
+++ meet-a-stranger/src/main/java/com/epam/training/student_Gagik_Hovhannisyan/meetastranger/MeetAStranger.java
@@ -0,0 +1,11 @@
+package com.epam.training.student_Gagik_Hovhannisyan.meetastranger;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
