e779250a0b13ee2e595e8f655b161efefcc5b25d
Gaghov <133580849+Gaghov@users.noreply.github.com>
2023-07-03 00:55:09 +0400

    Practical Task "Validations Epam Email"

---
+++ validations-epam-email/src/main/java/com/epam/training/student_Gagik_Hovhannisyan/validations/EpamEmailValidation.java
@@ -0,0 +1,15 @@
+package com.epam.training.student_Gagik_Hovhannisyan.validations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EpamEmailValidation {
+    public static boolean validateEpamEmail(String email) {
+        if (email == null) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile("[^_][a-z]*_[a-z-][^_]*[\\d]?@epam.com");
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
