78daef70aef6a3cf1e6ff8fce21da1d09a9ab434
Gaghov <133580849+Gaghov@users.noreply.github.com>
2023-07-02 23:56:48 +0400

    Practical Task "Figures"

---
+++ figures/src/main/java/com/epam/training/student_Gagik_Hovhannisyan/figures/Figure.java
@@ -0,0 +1,14 @@
+package com.epam.training.student_Gagik_Hovhannisyan.figures;
+
+abstract class Figure{
+
+    public abstract double area();
+
+    public abstract String pointsToString();
+
+    public String toString() {
+        return this.getClass().getSimpleName() + "[" + pointsToString() + "]";
+    }
+
+    public abstract Point leftmostPoint();
+}
